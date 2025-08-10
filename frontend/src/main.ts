f10f1e464be46afc5a7d81d35de9b6fac7960680
ilmir khusnutdinov <ilmir.khusnutdinov.kh@gmail.com>
2025-08-11 02:59:40 +0500

    init: инициализация клиентской части приложения
    - развернул проект с помощью команды npm create vue@latest;
    - удалил ненужные файлы;
    - прописал правила для eslint;
    - прописал правила для prettier;
    - установил css-препроцессор sass;
    - настроил alias-пути, которые могут пригодиться;
    - добавил favicon для приложения.

---
+++ frontend/src/main.ts
@@ -0,0 +1,11 @@
+import { createPinia } from "pinia";
+import { createApp } from "vue";
+
+import App from "./App.vue";
+
+const app = createApp(App);
+const pinia = createPinia();
+
+app.use(pinia);
+
+app.mount("#app");
---
+++ frontend/vite.config.ts
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vueDevTools from "vite-plugin-vue-devtools";
+
+import vue from "@vitejs/plugin-vue";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueDevTools()],
+  resolve: {
+    alias: {
+      "assets/": fileURLToPath(new URL("./src/assets/", import.meta.url)),
+      "styles/": fileURLToPath(new URL("./src/styles/", import.meta.url)),
+      "ui/": fileURLToPath(new URL("./src/components/ui/", import.meta.url)),
+      "shared/": fileURLToPath(new URL("./src/components/shared/", import.meta.url)),
+      "common/": fileURLToPath(new URL("./src/components/common/", import.meta.url)),
+      "composable/": fileURLToPath(new URL("./src/composable/", import.meta.url)),
+      "api/": fileURLToPath(new URL("./src/api/", import.meta.url)),
+      "store/": fileURLToPath(new URL("./src/store/", import.meta.url)),
+      "modules/": fileURLToPath(new URL("./src/modules/", import.meta.url)),
+    },
+  },
+});
